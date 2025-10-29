c0afb66266e1193662caeb32eb046a73e3d4c672
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-29 11:01:53 -0300

    xdf

--- springcloud/src/main/java/cloudspring/springcloud/DTO/PatientDto.java
+++ springcloud/src/main/java/cloudspring/springcloud/DTO/PatientDto.java
@@ -1,4 +1,5 @@
 package cloudspring.springcloud.DTO;
 
+
 public class PatientDto {
 }
--- springcloud/src/main/java/cloudspring/springcloud/Repository/PatientRepository.java
+++ springcloud/src/main/java/cloudspring/springcloud/Repository/PatientRepository.java
@@ -1,4 +1,7 @@
 package cloudspring.springcloud.Repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class PatientRepository {
 }
