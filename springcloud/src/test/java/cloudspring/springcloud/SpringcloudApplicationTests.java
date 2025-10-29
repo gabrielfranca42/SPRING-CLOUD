06323f1ec4cb0ff9aae41cd14509327868f54e0f
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-29 10:11:00 -0300

    criando ambiente

---
+++ springcloud/src/test/java/cloudspring/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package cloudspring.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
