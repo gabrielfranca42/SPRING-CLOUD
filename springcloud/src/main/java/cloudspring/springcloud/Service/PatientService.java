55a6d24a402b5c019851ddf7742e88bd22f2ca60
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-31 11:08:51 -0300

    criando service buildando mas sem kafka

--- springcloud/src/main/java/cloudspring/springcloud/Service/PatientService.java
+++ springcloud/src/main/java/cloudspring/springcloud/Service/PatientService.java
@@ -1,23 +1,30 @@
 package cloudspring.springcloud.Service;
 
 import cloudspring.springcloud.DTO.PatientResponseDto;
+import cloudspring.springcloud.Mapper.PatientMapper;
 import cloudspring.springcloud.Model.PatientModel;
+import cloudspring.springcloud.Repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PatientService {
-    private PatientService patientRepository;
+    private PatientRepository patientRepository;
 
-    public PatientService(PatientService patientRepository){
+    public PatientService(PatientRepository patientRepository){
         this.patientRepository = patientRepository;
     }
 
     public List<PatientResponseDto> getPatientModel(){
         List<PatientModel> patientModel = patientRepository.findAll();
 
-        List<PatientResponseDto> patientResponseDtos = patientModel.stream()
-                .map(Pa)
+        List<PatientResponseDto> patientResponseDtos =
+                patientModel.stream()
+                        .map(PatientMapper::toDTO).toList();
+
+
+        return patientResponseDtos;
     }
+
 }
