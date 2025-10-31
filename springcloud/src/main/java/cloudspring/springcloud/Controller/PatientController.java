5d3220f40af481277b92d931da4e1971948027ac
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-31 11:16:52 -0300

    criando controller mas sem kafka deve ser criado ainda buildando

--- springcloud/src/main/java/cloudspring/springcloud/Controller/PatientController.java
+++ springcloud/src/main/java/cloudspring/springcloud/Controller/PatientController.java
@@ -1,7 +1,26 @@
 package cloudspring.springcloud.Controller;
 
+import cloudspring.springcloud.DTO.PatientResponseDto;
+import cloudspring.springcloud.Service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/patients")
 public class PatientController {
+    private PatientService patientService;
+
+    public PatientController(PatientService patientService){
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDto>> getPatients(){
+        List<PatientResponseDto> patientsModel = patientService.getPatientModel();
+        return ResponseEntity.ok().body(patientsModel);
+    }
 }
