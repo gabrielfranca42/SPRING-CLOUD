b41b6cfff3aeda64228fb87172897d4fc16f39b2
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-11-03 10:07:20 -0300

    criando PatientRequestDto

---
+++ springcloud/src/main/java/cloudspring/springcloud/DTO/PatientRequestDto.java
@@ -0,0 +1,70 @@
+package cloudspring.springcloud.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDto {
+    public PatientRequestDto(String name) {
+        this.name = name;
+    }
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 100,message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Date of birth is required")
+    private String registeredDate;
+
+    public PatientRequestDto(String name, String email, String address, String registeredDate) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.registeredDate = registeredDate;
+    }
+
+
+    public PatientRequestDto(){
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+}
