33b0f699adc99acbd0ff78c1d1b711c4e30e4e03
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-11-04 09:37:14 -0300

    apenas adicionando  mais um PostMapping

--- springcloud/src/main/java/cloudspring/springcloud/Controller/PatientController.java
+++ springcloud/src/main/java/cloudspring/springcloud/Controller/PatientController.java
@@ -1,11 +1,12 @@
 package cloudspring.springcloud.Controller;
 
+import cloudspring.springcloud.DTO.PatientRequestDto;
 import cloudspring.springcloud.DTO.PatientResponseDto;
 import cloudspring.springcloud.Service.PatientService;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +23,12 @@ public class PatientController {
     public ResponseEntity<List<PatientResponseDto>> getPatients(){
         List<PatientResponseDto> patientsModel = patientService.getPatientModel();
         return ResponseEntity.ok().body(patientsModel);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDto> createPatientModel(@Valid @RequestBody PatientRequestDto patientRequestDto){
+      PatientResponseDto patientResponseDto = patientService.createPatient(patientRequestDto);
+
+      return ResponseEntity.ok().body(patientResponseDto);
     }
 }
