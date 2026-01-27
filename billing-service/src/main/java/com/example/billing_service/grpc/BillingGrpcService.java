f143b53526eed3384ace5d17166fda54e975a84c
BILLY BAT <gabrielfranca172@gmail.com>
2026-01-27 16:04:53 -0300

    ja tou puto nesse caralho

--- billing-service/src/main/java/com/example/billing_service/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/example/billing_service/grpc/BillingGrpcService.java
@@ -1,4 +1,17 @@
-package com.example.billing_service.grpc;
+package com.example.billing_service.grpc; // Altere de 'pm' para 'example' // Verifique se o seu package está correto aqui
 
-public class BillingGrpcService {
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver){
+
+    }
+
 }
